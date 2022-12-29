bbf36c5453814109d495baf174c55a3073dadb3d
Stephen-Sim <simshansiong2002@gmail.com>
2022-12-29 15:53:34 +0800

    fix bug

--- CarPoolingDriverAPP/CarPoolingDriverAPP/ViewModels/Auth/LoginPageViewModel.cs
+++ CarPoolingDriverAPP/CarPoolingDriverAPP/ViewModels/Auth/LoginPageViewModel.cs
@@ -14,8 +14,22 @@ namespace CarPoolingDriverAPP.ViewModels.Auth
     {
         public AuthService authService { get; set; }
 
-        public string Username { get; set; } = null;
-        public string Password { get; set; } = null;
+        private string username = string.Empty;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; OnPropertyChanged(); }
+        }
+
+        private string password = string.Empty;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; OnPropertyChanged(); }
+        }
+
         public ICommand SignInButtonClick
         {
             get
@@ -40,6 +54,8 @@ namespace CarPoolingDriverAPP.ViewModels.Auth
                     {
                         await Application.Current.MainPage.DisplayAlert("Alert", "Successfully Login!!", "Ok");
                         await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+                        Username = string.Empty;
+                        Password = string.Empty;
                     }
                     else
                     {
